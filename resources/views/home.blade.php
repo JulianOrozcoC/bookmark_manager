99d88ed8a1609cad4910993c5f030bcbf7f0ccc0
Julian Orozco <orozco.castorena@outlook.com>
2018-02-20 00:45:43 -0600

    add bookmark functionality

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -13,11 +13,41 @@
                             {{ session('status') }}
                         </div>
                     @endif
-
-                    You are logged in!
+                    @include('inc.messages')
+                    <button class="btn btn-primary bnt-lg" data-toggle="modal" data-target="#addModal" type="button" name="button"> Add Bookmark </button>
                 </div>
             </div>
         </div>
     </div>
+</div>
+<div class="modal fade" id="addModal">
+  <div class="modal-dialog" role="document">
+    <div class="modal-content">
+      <div class="modal-header">
+        <h5 class="modal-title">Add Bookmark</h5>
+        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+          <span aria-hidden="true">&times;</span>
+        </button>
+      </div>
+      <div class="modal-body">
+        <form class="" action="{{ route('bookmarks.store') }}" method="post">
+            {{csrf_field()}}
+            <div class="form-control">
+                <label>Bookmark Name:</label>
+                <input type="text" name="name" class="form-control">
+            </div>
+            <div class="form-control" style="margin-top: 15px;">
+                <label>Bookmark URL:</label>
+                <input type="text" name="url" class="form-control">
+            </div>
+            <div class="form-control" style="margin-top: 15px;">
+                <label>Bookmark Descriptiom:</label>
+                <textarea name="description" class="form-control"></textarea>
+            </div>
+            <input type="submit" name="submit" value="Submit" class="btn btn-success" style="margin-top: 15px;">
+        </form>
+      </div>
+    </div>
+  </div>
 </div>
 @endsection
